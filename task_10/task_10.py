d27875bd8317be378312c9cce58481b2c94d56e9
Alex Agafonov <taganroglad1@gmail.com>
2023-07-22 08:29:56 +0300

    just added task 10 code

--- task_10/task_10.py
+++ task_10/task_10.py
@@ -1,1 +1,10 @@
-print("helloooooo")
+n = int(input("Введите количество монеток (n): "))
+coins = input("Введите последовательность монеток (например, 'HHHTTT'): ")
+
+heads = coins.count('H')
+tails = n - heads
+
+min_flips = min(heads, tails)
+
+
+print(f"Минимальное количество монет, которые нужно перевернуть: {min_flips}")
