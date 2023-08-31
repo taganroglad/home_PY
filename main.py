98c3ac39d11049de8c9113cdcda4b597e37bf19f
Alex Agafonov <taganroglad1@gmail.com>
2023-08-31 19:23:01 +0300

    just have added 44 folder

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import random
 
-file_path = 'sample_data/california_housing_train.csv'
-data = pd.read_csv(file_path)
+# Создаем DataFrame
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
 
-filtered_data = data[(data['population'] >= 0) & (data['population'] <= 500)]
-average_house_value = filtered_data['median_house_value'].mean()
-print("Средняя стоимость дома с количеством людей от 0 до 500:", average_house_value)
+# Преобразуем в one-hot представление
+one_hot_data = data.pivot_table(index=data.index, columns='whoAmI', aggfunc=len, fill_value=0)
+
+print(one_hot_data)
