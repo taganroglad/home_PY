55644b36961ef79fd1a0257ec12d27532ec4e1cc
Alex Agafonov <taganroglad1@gmail.com>
2023-08-31 18:50:23 +0300

    just have added 40th folder

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def sum(a, b):
-    return a + b
+import pandas as pd
 
+file_path = 'sample_data/california_housing_train.csv'
+data = pd.read_csv(file_path)
+
+filtered_data = data[(data['population'] >= 0) & (data['population'] <= 500)]
+average_house_value = filtered_data['median_house_value'].mean()
+print("Средняя стоимость дома с количеством людей от 0 до 500:", average_house_value)
